b0af7ce047a3827970b7a664aef4737111e242aa
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-05-22 18:34:20 -0400

    adding code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("utils"); // is this required if I am also using the line below?
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+// questions are going to be objects
+// function writeToFile(fileName, data) {}
+// async function promptUser() {
+// function promptUser() {
+//     return inquirer.prompt([
+// function writeToFile(fileName, data) {}
+const questions =
+    ([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your ReadMe?",
+        },
+        {
+            type: "input",
+            name: "describe",
+            message: "Describe your project?",
+        },
+        {
+            type: "input",
+            name: "instuction",
+            message: "What are the installation instructions for your project?",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is the intended usage for your project?",
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "What are the contribution guidelines for your project?",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Select a license from the list",
+            choices: ["MIT", "Mozilla", "Apache", "GNU APLv3", "GNU GPLv3", "GNU LGPLv3", "Boost", "Unilicense"],
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your github username?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",
+        },
+    ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log("success!")
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    promptUser()
+    inquirer.prompt(questions)
+        .then(function (answers) {
+            const text = generateMarkdown.generateMarkdown(answers)
+            writeToFile("README.md", text)
+        })
+}
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,81 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link;
+  switch (license) {
+    case "GNU_AGPLv3": link = "https://choosealicense.com/licenses/agpl-3.0/"
+      break;
+    case "GNU_GPLv3": link = "https://choosealicense.com/licenses/gpl-3.0/"
+      break;
+    case "GNU LGPLv3": link = "https://choosealicense.com/licenses/lgpl-3.0/"
+      break;
+    case "Mozilla": link = "https://choosealicense.com/licenses/mpl-2.0/"
+      break;
+    case "Apache": link = "https://choosealicense.com/licenses/apache-2.0/"
+      break;
+    case "MIT": link = "https://choosealicense.com/licenses/mit/"
+      break;
+    case "Boost": link = "https://choosealicense.com/licenses/bsl-1.0/"
+      break;
+    case "Unilicense": link = "https://choosealicense.com/licenses/unlicense/"
+      break;
+    default: link = "no licencse";
+  }
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (licence === 'MIT') {
+    return " ";
+  } else {
+    return `[![${license}](https://img.shields.io/badge/license-${license}-blue)](${renderLicenseLink(license)})`
+  }
+  // TODO: Create a function to generate markdown for README
+  function generateMarkdown(data) {
+    // console.log(data.title);
+    // pesky back tick placement
+  return `${renderLicenseBadge(data.licence)}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+# ${data.title};
 
+# Table of Contents
+-[Description](#Description)
+-[Installation](#Installation)
+-[Usage](#Usage)
+-[Contributors](#Contributors)
+-[License](#License)
+-[Username](#Github)
+-[Email](#Email)
+
+# Description
+${data.description}
+
+# Installation
+${data.installation}
+
+# Usage
+${data.usage}
+
+# License
+${data.licence}
+
+# Contributors
+${data.contributors}
+
+# Username
+${data.github}
+
+# Email
+${data.email}
 `;
+  }
 }
-
 module.exports = generateMarkdown;
+// using link instead of badge, clean it up
